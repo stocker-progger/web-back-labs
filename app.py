fe224b10cdc87a8ff547189246cd630d054bcecf
Данил Трохин <tdv2004t@mail.ru>
2024-09-11 16:55:20 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def start():
+@app.route("/web")
+def web():
     return """<!dostype html>
         <html>
            <body>
                <h1>web-сервер на flask<h1>
+               <a href="/author">author</a>
            </body>
         </html>"""
 
+@app.route("/author")
+def author():
+    name = "Трохин Данил Витальевич"
+    group = "ФБИ-21"
+    faculty = "ФБ"
+    
+    return """<!dostype html>
+        <html>
+           <body>
+               <p>Студент: """ + name + """<p>
+               <p>Студент: """ + group + """<p>
+               <p>Студент: """ + faculty + """<p>
+               <a href="/web">web</a>
+           </body>
+        </html>"""

559c27e5d10f55a18524ba5e7da5de1b7c59f087
Данил Трохин <tdv2004t@mail.ru>
2024-09-22 12:27:33 +0700

    Слэши в адресе

--- app.py
+++ app.py
@@ -467,3 +467,11 @@ def resource_status():
         </body>
     </html>
     '''
+@app.route('/lab2/a')
+def a1():
+    return 'без слэша'
+
+@app.route('/lab2/a/')
+def a2():
+    return 'со слэшем'
+

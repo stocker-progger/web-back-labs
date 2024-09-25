a92cf04aa841973bda74747a36520eb7f44f2224
Данил Трохин <tdv2004t@mail.ru>
2024-09-25 22:42:11 +0700

    настройки изображения

--- lab3.py
+++ lab3.py
@@ -21,9 +21,9 @@ def cookie():
 @lab3.route('/lab3/del_cookie')
 def del_cookie():
     resp = make_response(redirect('/lab3/'))
-    resp.delete_cookie('name', 'Alex', max_age=5)
-    resp.delete_cookie('age', '20')
-    resp.delete_cookie('name_color', 'magenta')
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
     return resp
 
 
@@ -73,3 +73,26 @@ def success():
     price = request.args.get('price', 0)  # Получаем цену из URL параметра
     return render_template('lab3/success.html', price=price)
 
+
+@lab3.route('/lab3/settings')
+def settings():
+    # Считываем параметры из запроса
+    color = request.args.get('color')
+    background = request.args.get('background')
+    font_size = request.args.get('font_size')
+    
+    # Если параметры переданы, сохраняем их в куки
+    if color or background or font_size:
+        resp = make_response(redirect('/lab3/settings'))
+        if color:
+            resp.set_cookie('color', color)
+        if background:
+            resp.set_cookie('background', background)
+        if font_size:
+            resp.set_cookie('font_size', font_size)
+        return resp
+
+    color = request.cookies.get('color')
+    background = request.cookies.get('background')
+    font_size = request.cookies.get('font_size')
+    return render_template('lab3/settings.html', color=color, background=background, font_size=font_size)

81a676396c28bf5d80f41f94c14f7f7c84063f36
Данил Трохин <tdv2004t@mail.ru>
2024-09-25 21:46:29 +0700

    заказ напитка

--- lab3.py
+++ lab3.py
@@ -39,4 +39,37 @@ def form1():
         errors['age'] = 'Заполните поле!'
 
     sex = request.args.get('sex')
-    return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    # Пусть кофе стоит 120 рублей, чёрный чай — 80 рублей, зелёный — 70 рублей.
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    
+    # Добавка молока удорожает напиток на 30 рублей, а сахара — на 10.
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    
+    return render_template('lab3/pay.html', price=price)
+
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price', 0)  # Получаем цену из URL параметра
+    return render_template('lab3/success.html', price=price)
+

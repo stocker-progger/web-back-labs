300d0229ee71b953c5162465703a49c781bdb025
Данил Трохин <tdv2004t@mail.ru>
2024-10-14 15:51:59 +0700

    Заготовка к 4-ой лабораторной

--- lab4.py
+++ lab4.py
@@ -3,11 +3,8 @@ from datetime import datetime
 lab4 = Blueprint('lab4', __name__)
 
 
-@lab3.route('/lab3/')
+@lab4.route('/lab4/')
 def laba3():
-    name = request.cookies.get('name', 'Анонимус') 
-    name_color = request.cookies.get('name_color')
-    age = request.cookies.get('age', ' ')
-    return render_template('lab3/lab3.html', name=name, name_color=name_color,  age=age)
+    return render_template('lab4/lab4.html')
 
 

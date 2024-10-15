9dbda782ab7c75373cfce489748c21e5a1fe10fe
Данил Трохин <tdv2004t@mail.ru>
2024-10-15 20:36:13 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -130,6 +130,14 @@ def tree():
     return redirect('/lab4/tree')
 
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'sofa', 'password': '222'},
+    {'login': 'momo', 'password': '333'}
+]
+
+
 @lab4.route('/lab4/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -138,8 +146,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('lab4/login.html', error='Успешная авторизация', login=login, authorized=False)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('lab4/login.html', error='Успешная авторизация', login=login, authorized=False)
 
     error = 'Неверные логин и/или пароль'
     return render_template('lab4/login.html', error=error,  authorized=False)

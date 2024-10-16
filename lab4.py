fd6fa1c68cc8ad9b62a7699403372dcfe3fd0ac5
Данил Трохин <tdv2004t@mail.ru>
2024-10-16 17:11:40 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -166,3 +166,31 @@ def login():
 def logout():
     session.pop('login', None)
     return redirect('/lab4/login')
+
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    if request.method == 'GET':
+        return render_template('lab4/fridge.html')
+
+    temperature = request.form.get('temperature')
+
+    if not temperature:
+        return render_template('lab4/fridge.html', error='Ошибка: не задана температура')
+
+    try:
+        temperature = int(temperature)
+    except ValueError:
+        return render_template('lab4/fridge.html', error='Ошибка: введите корректное значение температуры')
+
+    if temperature < -12:
+        return render_template('lab4/fridge.html', error='Не удалось установить температуру — слишком низкое значение')
+    elif temperature > -1:
+        return render_template('lab4/fridge.html', error='Не удалось установить температуру — слишком высокое значение')
+    elif -12 <= temperature <= -9:
+        return render_template('lab4/fridge.html', temperature=temperature, snowflakes=3)
+    elif -8 <= temperature <= -5:
+        return render_template('lab4/fridge.html', temperature=temperature, snowflakes=2)
+    elif -4 <= temperature <= -1:
+        return render_template('lab4/fridge.html', temperature=temperature, snowflakes=1)
+
+    return render_template('lab4/fridge.html')

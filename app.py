6c3412f93eb4d9cd34a656ff7e627084c48edfba
Данил Трохин <tdv2004t@mail.ru>
2024-11-13 21:32:35 +0700

    Регистрация

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, url_for, redirect, render_template
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 
 
 app = Flask(__name__)
@@ -13,6 +15,7 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 
 @app.errorhandler(404)
@@ -62,6 +65,7 @@ def index():
                     <li><a href="/lab2/">Вторая лабораторная</a></li>
                     <li><a href="/lab3/">Третья лабораторная</a></li>
                     <li><a href="/lab4/">Четвертая лабораторная</a></li>
+                    <li><a href="/lab5/">Пятая лабораторная</a></li>
                 </ol>
             </div>
         </main>
---
+++ lab5.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, url_for, request, redirect, render_template, session
+lab5 = Blueprint('lab5', __name__)
+import psycopg2
+
+@lab5.route('/lab5/')
+def laba5():
+    username = 'Anonymous'
+    return render_template('lab5/lab5.html', username = username)
+
+
+@lab5.route('/lab5/login')
+def login():
+    return render_template('lab5/login.html')
+
+
+@lab5.route('/lab5/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login and password):
+        return render_template('lab5/register.html', error='Заполните все поля')
+
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'danil_trokhin_knowledge_base',
+        user = 'danil_trokhin_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template('lab5/register.html', error="Такой пользователь уже существует")
+
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html', login=login)
+
+
+@lab5.route('/lab5/list')
+def list_articles():
+    return render_template('lab5/list.html')
+
+
+@lab5.route('/lab5/create')
+def create_article():
+    return render_template('lab5/create.html')
+
+

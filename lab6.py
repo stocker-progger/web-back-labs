544b3636a614d1da9e8ec07fe4a0233f187cde2f
Данил Трохин <tdv2004t@mail.ru>
2024-11-19 10:33:53 +0700

    Бронирование офисов

--- lab6.py
+++ lab6.py
@@ -1,8 +1,65 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, request
 lab6 = Blueprint('lab6', __name__)
 
+
+offices = []
+for i in range(1, 11):
+    offices.append({"number": i, "tenant": ""})
 
 @lab6.route('/lab6/')
 def laba6():
     return render_template('lab6/lab6.html')
 
+
+@lab6.route('/lab6/json-rpc-api/', methods=['POST'])  
+def api():
+    data = request.get_json()  
+    id = data['id']
+    if data['method'] == 'info':
+        return {
+            'jsonrpc': '2.0',
+            'result': offices,
+            'id': id
+        }
+    #elif data['method'] == 'booking':
+    
+    login = session.get('login')
+    if not login:
+        return {
+            'jsonrpc': '2.0',
+            'error': {
+                'code': 1,
+                'message': 'Unauthorized'
+            },
+            'id': id
+        }
+    
+    if data['method'] == 'booking':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] != '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 2,
+                            'message': 'Already booked'
+                        },
+                        'id': id
+                    }
+                
+                office['tenant'] = login
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+
+    return {
+        'jsonrpc': '2.0',
+        'error': {
+            'code': -32601,
+            'message': 'Method not found'
+        },
+        'id': id
+    }

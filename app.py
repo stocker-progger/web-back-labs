d3b36bfdbaa5fe3e9f10a18f8bb790dc9c0de812
Данил Трохин <tdv2004t@mail.ru>
2024-12-15 20:33:12 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, url_for, redirect, render_template
-
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
 
 from lab1 import lab1
 from lab2 import lab2
@@ -17,6 +19,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRT_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 #app.secret_key = 'секретно-секретный секрет'
+
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'trohin_danil_orm'
+    db_user = 'trohin_danil_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "trohin_danil_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
